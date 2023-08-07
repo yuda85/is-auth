e64f5def62fe85107784146d4d5845d851829b76
yehuda buzaglo <yehudabu@rafael.co.il>
2023-08-07 12:26:30 +0300

    new api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCle8gtdJ3gurd51gWuRgmNOcLEIm0Fyjc',
-    authDomain: 'is-auth-app.firebaseapp.com',
-    projectId: 'is-auth-app',
-    storageBucket: 'is-auth-app.appspot.com',
-    messagingSenderId: '696341369255',
-    appId: '1:696341369255:web:10b6b50470e60b46072682',
-    measurementId: 'G-4W65ZYP3LS',
+    apiKey: 'AIzaSyAms2sKXrLvo_xVilVdZn31EjFWZS5E2QE',
+    authDomain: 'habits-76131.firebaseapp.com',
+    databaseURL: 'https://habits-76131.firebaseio.com',
+    projectId: 'habits-76131',
+    storageBucket: 'habits-76131.appspot.com',
+    messagingSenderId: '1064921247045',
+    appId: '1:1064921247045:web:f8a8c765a79c84c049a22c',
+    measurementId: 'G-V5YJKWQXNV',
   },
 };
