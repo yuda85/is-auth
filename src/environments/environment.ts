5a6d0204f05e3b9baac87a6a3b1d7efd0a1e3a31
yehuda buzaglo <yehudabu@rafael.co.il>
2023-08-07 11:25:39 +0300

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  }
+    apiKey: 'AIzaSyCle8gtdJ3gurd51gWuRgmNOcLEIm0Fyjc',
+    authDomain: 'is-auth-app.firebaseapp.com',
+    projectId: 'is-auth-app',
+    storageBucket: 'is-auth-app.appspot.com',
+    messagingSenderId: '696341369255',
+    appId: '1:696341369255:web:10b6b50470e60b46072682',
+    measurementId: 'G-4W65ZYP3LS',
+  },
 };
